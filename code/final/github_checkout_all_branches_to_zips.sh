3c6059a7b25bf5219ae2f17936cea28f3573e431
xiuxi <xiuxi554325746@gmail.com>
2021-09-29 01:21:02 +0800

    a

--- code/final/github_checkout_all_branches_to_zips.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for branch in $(git branch --all | grep '^\s*remotes' | egrep --invert-match '(:?HEAD|master)$'); do
-	git branch --track "${branch##*/}" "$branch"
-	git checkout ${branch##*/}
-	value=${branch##*/}
-	zip -r ${value}.zip . -x '*.git*' -x '*.zip*' 'a.sh' 'a.txt'
-done
